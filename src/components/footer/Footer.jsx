8f9c4eaed7e5fc46b64ae0f24de4de3fcf36e8a5
Katherine <katherine@latamready.com>
2023-04-08 23:55:48 -0500

    Production changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
       <div className='footer-social'>
         <a href="https://www.linkedin.com/in/katherine-miraval/" target='_blank' rel="noreferrer"><FaLinkedinIn /></a>
         <a href="https://github.com/kattymmc" target='_blank' rel="noreferrer"><FaGithub /></a>
-        <a href="https://www.instagram.com/kattymmc/" target='_blank'><FaInstagram /></a>
+        <a href="https://www.instagram.com/kattymmc/" target='_blank' rel="noreferrer"><FaInstagram /></a>
       </div>
       <div className='footer-text'>
         Copyright © | Hecho en React por Katherine Miraval
